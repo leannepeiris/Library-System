d801dc11da3cb7399160d54c4e517c7feda87607
Leanne Peiris <leannelois203@gmail.com>
2018-10-05 16:58:48 +0530

    git ignore file and the connection file with the tables and the database

--- connection.php
+++ connection.php
@@ -7,29 +7,28 @@ $dbname = "library";
 
 $conn = mysqli_connect($dbhost, $dbuser, $dbpass);
 
-if(!$conn) {
+if(!$conn)
+{
     die("Could not Connect: ". mysqli_error($conn)); echo "<br/>";
-} //echo "Connected Successfully"; echo "<br/>";
+}
 mysqli_select_db($conn,$dbname);
 
-// Create database
 $sql = "CREATE DATABASE IF NOT EXISTS ". $dbname;
 if ($conn->query($sql) === TRUE) {
     //echo "Database created successfully"; echo "<br/>";
 } else {
-
     echo "Error creating database: " . $conn->error; echo "<br/>";
 }
 
 $conn = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname);
 
-// creating the tables in the database
-//employee table
 $employees = "CREATE TABLE IF NOT EXISTS employees (
 id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY, 
-firstname VARCHAR(191) NOT NULL,
-lastname VARCHAR(191) NOT NULL,
+firstname VARCHAR(191),
+lastname VARCHAR(191),
 email VARCHAR(191),
+contact_number VARCHAR(191),
+date_of_birth VARCHAR(191),
 created_at TIMESTAMP
 )";
 
@@ -39,11 +38,11 @@ if ($conn->query($employees) === TRUE) {
     echo "Error creating table: " . $conn->error; echo "<br/>";
 }
 
-//books table
 $books = "CREATE TABLE IF NOT EXISTS  books (
 id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY, 
 title VARCHAR(191) NOT NULL,
-author VARCHAR(191) NOT NULL,
+genre VARCHAR(191),
+author VARCHAR(191),
 publisher VARCHAR(191),
 created_at TIMESTAMP
 )";
@@ -53,3 +52,65 @@ if ($conn->query($books) === TRUE) {
 } else {
     echo "Error creating table: " . $conn->error; echo "<br/>";
 }
+
+$authors = "CREATE TABLE IF NOT EXISTS  authors (
+id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY, 
+firstname VARCHAR(191),
+lastname VARCHAR(191),
+genre VARCHAR(191),
+publisher VARCHAR(191),
+created_at TIMESTAMP
+)";
+
+if ($conn->query($authors) === TRUE) {
+    //echo "Table Authors created successfully"; echo "<br/>";
+} else {
+    echo "Error creating table: " . $conn->error; echo "<br/>";
+}
+
+$publishers = "CREATE TABLE IF NOT EXISTS  publishers (
+id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY, 
+name VARCHAR(191),
+city VARCHAR(191,
+created_at TIMESTAMP
+)";
+
+if ($conn->query($publishers) === TRUE) {
+    //echo "Table publishers created successfully"; echo "<br/>";
+} else {
+    echo "Error creating table: " . $conn->error; echo "<br/>";
+}
+
+$customers = "CREATE TABLE IF NOT EXISTS  customers (
+id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY, 
+firstname VARCHAR(191) NOT NULL,
+lastname VARCHAR(191) NOT NULL,
+email VARCHAR(191),
+contact_number VARCHAR(191),
+created_at TIMESTAMP
+)";
+
+if ($conn->query($customers) === TRUE) {
+    //echo "Table customers created successfully"; echo "<br/>";
+} else {
+    echo "Error creating table: " . $conn->error; echo "<br/>";
+}
+
+$borrowed_books = "CREATE TABLE IF NOT EXISTS  borrowed_books (
+id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY, 
+book VARCHAR(191) NOT NULL,
+customer VARCHAR(191) NOT NULL,
+borrowed_date TIMESTAMP,
+due_date VARCHAR(191),
+overdue VARCHAR(191),
+overdue_charge VARCHAR(191),
+fine VARCHAR(191),
+created_at TIMESTAMP
+)";
+
+if ($conn->query($borrowed_books) === TRUE) {
+    //echo "Table borrowed_books created successfully"; echo "<br/>";
+} else {
+    echo "Error creating table: " . $conn->error; echo "<br/>";
+}
+
